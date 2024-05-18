c2207b76459e9f54ade1aa04b6ad5f0658f3626d
Malsha-Weerasinghe <weerasinghemalsha4@gmail.com>
2024-05-18 16:02:38 +0530

    Applied tailwind css

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,16 @@ import './App.css';
 
 function App() {
   return (
-   <h1>This is App.js</h1>
+      <div>
+         <h1 className={"text-5xl text-red-500"}>This is App.js</h1>
+
+        <div style={{backgroundColor:"red", padding:10, textAlign:"center"}}>Inline CSS Box</div>
+          <br/>
+          <div className={"box"}>External CSS Box</div>
+          <br/>
+          <div className={"bg-red-500 p-3 text-center"}> Utility Box</div>
+      </div>
+
   );
 }
 
